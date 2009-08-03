a22e3e38510d938218c5675cd8bd44912e9bfaf4
Zachary Voase <zacharyvoase@me.com>
2009-08-03 22:12:19 +0100

    Added short description to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distutils.core import setup
 setup(
     name='DjanJinja',
     version='0.5',
+    description='Easy Django and Jinja2 integration.',
     packages=['djanjinja', 'djanjinja.bundles', 'djanjinja.extensions'],
     url='http://bitbucket.org/zacharyvoase/djanjinja/',
     
